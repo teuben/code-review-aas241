1a5b0690d0b4859de383f6436c28301856b64e97
Erick Aguirre <ea49610@gmail.com>
2023-01-08 15:53:19 -0800

    fixed indent error

--- my_plot.py
+++ my_plot.py
@@ -1,33 +1,33 @@
 import numpy as np
- import matplotlib
- import os
- from astropy.io import fits
- import glob
- data = fits.getdata('/Users/bostroem/Desktop/images/hubble_img.fits')
- col1 = data[20, :]
- col2 = data[200, :]
- col3 = data[800, :]
- pyplot.figure()
- pyplot.plot(np.arange(1024), col1)
- mean = np.mean(col1)
- pyplot.axhline(mean)
- pyplot.legend(['Column 20', 'Mean'])
- pyplot.title('Plot of Column 20 Values and Mean')
- pyplot.xlabel('Pixels')
- pyplot.ylabel('Intensity')
- pyplot.figure()
- pyplot.plot(np.arange(1024), col2)
- mean = np.mean(col2)
- pyplot.axhline(mean)
- pyplot.legend(['Column 200', 'Mean'])
- pyplot.title('Plot of Column 200 Values and Mean')
- pyplot.xlabel('Pixels')
- pyplot.ylabel('Intensity')
- pyplot.figure()
- pyplot.plot(np.arange(1024), col3)
- mean = np.mean(col3)
- pyplot.axhline(mean)
- pyplot.legend(['Column 800', 'Mean'])
- pyplot.title('Plot of Column 800 Values and Mean')
- pyplot.xlabel('Pixels')
- pyplot.ylabel('Intensity')
+import matplotlib
+import os
+from astropy.io import fits
+import glob
+data = fits.getdata('/Users/bostroem/Desktop/images/hubble_img.fits')
+col1 = data[20, :]
+col2 = data[200, :]
+col3 = data[800, :]
+pyplot.figure()
+pyplot.plot(np.arange(1024), col1)
+mean = np.mean(col1)
+pyplot.axhline(mean)
+pyplot.legend(['Column 20', 'Mean'])
+pyplot.title('Plot of Column 20 Values and Mean')
+pyplot.xlabel('Pixels')
+pyplot.ylabel('Intensity')
+pyplot.figure()
+pyplot.plot(np.arange(1024), col2)
+mean = np.mean(col2)
+pyplot.axhline(mean)
+pyplot.legend(['Column 200', 'Mean'])
+pyplot.title('Plot of Column 200 Values and Mean')
+pyplot.xlabel('Pixels')
+pyplot.ylabel('Intensity')
+pyplot.figure()
+pyplot.plot(np.arange(1024), col3)
+mean = np.mean(col3)
+pyplot.axhline(mean)
+pyplot.legend(['Column 800', 'Mean'])
+pyplot.title('Plot of Column 800 Values and Mean')
+pyplot.xlabel('Pixels')
+pyplot.ylabel('Intensity')
