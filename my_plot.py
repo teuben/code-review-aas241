979fdd73dd394d6d7e77f99506edcd70e919efc4
Peter Teuben <teuben@gmail.com>
2023-01-09 11:18:59 -0500

    make the NX dimension extracted from the data

--- my_plot.py
+++ my_plot.py
@@ -10,12 +10,13 @@ import glob
 fitsfile = 'https://github.com/teuben/AAS241/raw/main/M100.mom0.fits'
 data = fits.getdata(fitsfile)
 
-def plot1(col_number,data):
+def plot_column(col_number,data):
     """ plot a column from a 2d matrix
     """
+    (nx,ny) = data.shape
     col =data[col_number, :]
     pyplot.figure()
-    pyplot.plot(np.arange(67), col)
+    pyplot.plot(np.arange(nx), col)
     mean = np.mean(col)
     pyplot.axhline(mean)
     pyplot.legend([f'Column {col_number}', 'Mean'])
@@ -23,8 +24,10 @@ def plot1(col_number,data):
     pyplot.xlabel('Pixels')
     pyplot.ylabel('Intensity')
 
-plot1(20, data)
-plot1(33,data)
-plot1(50,data)
+print(f"3 columns in {fitsfile}")
+
+plot_column(20,data)
+plot_column(33,data)
+plot_column(50,data)
 
 pyplot.show()
